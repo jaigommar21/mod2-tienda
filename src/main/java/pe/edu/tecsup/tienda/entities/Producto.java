5658ef473366c568711601148afdee227d10a813
Jaime <jgomezm@tecsup.edu.pe>
2021-08-10 21:05:59 -0500

    update entities package

---
+++ src/main/java/pe/edu/tecsup/tienda/entities/Producto.java
@@ -0,0 +1,27 @@
+package pe.edu.tecsup.tienda.entities;
+
+public class Producto {
+
+private Integer id;
+	
+	private Integer categorias_id;
+	
+	private Categoria categoria;
+	
+	private String nombre;
+	
+	private String descripcion;
+	
+	private Double precio;
+	
+	private Integer stock;
+	
+	private String imagen_nombre;
+	
+	private String imagen_tipo;
+	
+	private Long imagen_tamanio;
+	
+	private Integer estado;
+
+}
