baeb43ac8c00d1610ed68d679f1b0592ab010ed1
Jaime <jgomezm@tecsup.edu.pe>
2021-08-12 21:14:44 -0500

    Update delete product

---
+++ src/main/java/pe/edu/tecsup/tienda/servlets/ProductoEliminarServlet.java
@@ -0,0 +1,49 @@
+package pe.edu.tecsup.tienda.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import pe.edu.tecsup.tienda.services.ProductoService;
+
+@WebServlet("/ProductoEliminarServlet")
+public class ProductoEliminarServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+    
+	private static final Logger log = Logger.getLogger(ProductoListarServlet.class);
+	
+	private ProductoService productoService;
+	
+	public ProductoEliminarServlet() {
+		this.productoService = new ProductoService();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		log.info("Call doGet()");
+		
+		try {
+			
+			Integer id = Integer.parseInt(request.getParameter("id"));
+			
+			productoService.eliminar(id);
+			
+			response.sendRedirect(request.getContextPath() + "/ProductoListarServlet");
+	        
+		} catch (Exception e) {
+			log.error(e, e);
+			throw new ServletException(e.getMessage(), e);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
